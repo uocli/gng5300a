e4be4e3727001ef62d4c310b79b54642f6976d3f
Sean Li <cli049@uottawa.ca>
2024-09-20 20:51:16 -0400

    Week 3 Activity 3

--- week3/week3_activities.py
+++ week3/week3_activities.py
@@ -1,12 +1,14 @@
-# Activity 1
+# Activity 1: Create a simple website
 # home.html
 
-# Activity 2
+# Activity 2: Login and registration pages
 # login.html
 # register.html
 
-# Activity 3
-
+# Activity 3: Personal Blog
+# index.html
+# login.html
+# register.html
 
 # Activity 4
 
